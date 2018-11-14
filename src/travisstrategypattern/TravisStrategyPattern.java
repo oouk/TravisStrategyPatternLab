7be17645ac9a5394eea57fa4860fde950ab45f5e
INFINITE <INFINITE@134.154.52.96>
2018-11-14 11:33:54 -0800

    Travis/Strategy pattern Lab Exercise

---
+++ src/travisstrategypattern/TravisStrategyPattern.java
@@ -0,0 +1,19 @@
+/**
+ * Program Name: Travis and Strategy Pattern
+ * Created Date: Nov 14, 2018
+ * @author Odom Ouk
+ * @version 1.0 
+ * Email: oouk@horizon.csueastbay.edu
+ */
+
+package travisstrategypattern; 
+import java.util.Scanner;
+
+
+public class TravisStrategyPattern {
+
+ public static void main(String[] args) {
+     System.out.println("Hello world!");
+
+    }
+}
